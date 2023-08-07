16b2879c2eb9d3c0da07d493f65de397d430f6bc
CYW <68589904+ChongYW@users.noreply.github.com>
2023-08-07 15:03:17 +0800

    v3

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,6 +8,50 @@ use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
+//    public function getSelectedValues(array $keys)
+//    {
+//        $apiResponse = Http::get('https://api.huobi.pro/market/history/kline?period=1day&size=1&symbol=trxusdt');
+//
+//        if ($apiResponse->successful()) {
+//            $apiData = $apiResponse->json();
+//
+//            $result = [];
+//
+//            if (isset($apiData['data'][0])) {
+//                foreach ($keys as $key) {
+//                    $value = data_get($apiData, "data.0.$key");
+//                    $result[$key] = $value;
+//                }
+//            }
+//
+//            Huobi::create($result);
+//
+//            return $result;
+//        }
+//
+//        return [
+//            'error' => 'Failed to fetch data from the API.',
+//        ];
+//    }
+//
+//    public function showSelectedValues()
+//    {
+//
+//        $selectedKeys = ['open', 'close', 'test'];
+//
+//        $results = $this->getSelectedValues($selectedKeys);
+//
+//        foreach ($results as $key => $value)
+//        {
+//            if (empty($value)){
+//                echo "$key : Not found<br>";
+//            }else{
+//                echo "$key : $value<br>";
+//            }
+//        }
+//    }
+
+// v2
     public function getSelectedValues(array $keys)
     {
         $apiResponse = Http::get('https://api.huobi.pro/market/history/kline?period=1day&size=1&symbol=trxusdt');
@@ -17,14 +61,10 @@ class ApiController extends Controller
 
             $result = [];
 
-            if (isset($apiData['data'][0])) {
-                foreach ($keys as $key) {
-                    $value = data_get($apiData, "data.0.$key");
-                    $result[$key] = $value;
-                }
+            foreach ($keys as $key) {
+                $value = data_get($apiData, $key);
+                $result[$key] = $value;
             }
-
-            Huobi::create($result);
 
             return $result;
         }
@@ -36,13 +76,11 @@ class ApiController extends Controller
 
     public function showSelectedValues()
     {
-
-        $selectedKeys = ['open', 'close', 'test'];
+        $selectedKeys = ['ts', 'data.0.open', 'test'];
 
         $results = $this->getSelectedValues($selectedKeys);
 
-        foreach ($results as $key => $value)
-        {
+        foreach ($results as $key => $value) {
             if (empty($value)){
                 echo "$key : Not found<br>";
             }else{
